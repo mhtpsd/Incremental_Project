4a237fd526a9edc0553b773e6e6d85af5a090ea0
username <wecp-dummy@gmail.com>
2024-07-23 04:52:37 +0000

    wecp initial commit

--- frontend/src/app/bank/components/transaction/transaction.component.ts
+++ frontend/src/app/bank/components/transaction/transaction.component.ts
@@ -1,4 +1,26 @@
 
 export class TransactionComponent {
- 
+    transactionId?: number;
+    accountId: string;
+    amount: number;
+    transactionDate: Date;
+    transactionType: string;
+
+    constructor(accountId: string, amount: number, transactionDate: Date, transactionId?: number) {
+        this.transactionId = transactionId;
+        this.accountId = accountId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+    }
+
+    displayInfo() {
+        console.log(`Transaction ID: ${this.transactionId}`);
+        console.log(`Account ID: ${this.accountId}`);
+        console.log(`Amount: ${this.amount.toFixed(2)}`);
+        console.log(`Transaction Date: ${this.transactionDate}`);
+        console.log("------");
+    }
 }
+
+const transaction = new TransactionComponent("1", 50.00, new Date(), 1);
+transaction.displayInfo();
